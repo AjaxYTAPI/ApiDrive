0d52b8467ad6f71643a9495faf46cb65afad9f2f
AjaxYTAPI <boboca.studio1234567@gmail.com>
2025-03-18 14:10:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify
 
-app = Flask(__name__) # -> __main__ ("Este arquivo")
+data = [
+    {
+        "id": 1,
+        "Nome": "Fulano",
+        "Idade": 18
+    },
+    {
+        "id": 2,
+        "Nome": "Maria",
+        "Idade": 26
+    }
+]
+
+app = Flask(__name__)
 
 @app.route("/")
 def HomePage():
-    return "<h1> Bem vindo a minha api </h1>"
+    return "<h1>Bem-vindo à minha API!</h1>"
 
-app.run(host="0.0.0.0")
+@app.route("/api")
+def api():
+    return jsonify(data)
+
+@app.route('/pesquisa/<int:id>', methods=['GET'])
+def pesquisar(id):
+    for i in data:
+        if i["id"] == id:
+            return jsonify(i)  # Retorna o item se encontrar pelo ID
+    return jsonify({"erro": f"nenhum resultado com o id: {id}...."}), 404  # Retorna erro se não encontrar
+
+# Rodar o servidor na porta padrão (5000) e disponível para todos
+app.run(host="0.0.0.0", port=5000)
